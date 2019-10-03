8fca9deb466f1a4b90629f0a2151228151aff9d8
Mike Dilger <mike@optcomp.nz>
2019-10-04 10:49:55 +1300

    Working, basic setup for NZ seasons

--- src/lib.rs
+++ src/lib.rs
@@ -52,9 +52,69 @@ fn svg(document: &Document) -> Result<Element, JsValue>
     svg.append_child(&circle)?;
 
     let now = Local::now();
-    let (x,y) = calpoint(Local.ymd(now.year(), 1, 1).and_hms(0,0,0));
-    let line = svg_line(document, x, y, 500.0, 500.0, "black", "1")?;
-    svg.append_child(&line)?;
+    for month in 1..12+1 {
+        let (x,y) = calpoint(Local.ymd(now.year(), month, 1).and_hms(0,0,0));
+        let stroke_width = match month {
+            3 | 6 | 9 | 12 => "3",
+            _ => "1",
+        };
+        let line = svg_line(document, x, y, 500.0, 500.0, "black", stroke_width)?;
+        svg.append_child(&line)?;
+    }
+
+    let jan = svg_text(document, 490.0, 20.0, "Jan")?;
+    svg.append_child(&jan)?;
+    let feb = svg_text(document, 729.0, 93.0, "Feb")?;
+    svg.append_child(&feb)?;
+    let mar = svg_text(document, 895.0, 270.0, "Mar")?;
+    svg.append_child(&mar)?;
+    let apr = svg_text(document, 960.0, 500.0, "Apr")?;
+    svg.append_child(&apr)?;
+    let may = svg_text(document, 895.0, 730.0, "May")?;
+    svg.append_child(&may)?;
+    let jun = svg_text(document, 729.0, 907.0, "Jun")?;
+    svg.append_child(&jun)?;
+    let jul = svg_text(document, 490.0, 980.0, "Jul")?;
+    svg.append_child(&jul)?;
+    let aug = svg_text(document, 241.0, 907.0, "Aug")?;
+    svg.append_child(&aug)?;
+    let sep = svg_text(document, 75.0, 730.0, "Sep")?;
+    svg.append_child(&sep)?;
+    let oct = svg_text(document, 10.0, 500.0, "Oct")?;
+    svg.append_child(&oct)?;
+    let nov = svg_text(document, 75.0, 270.0, "Nov")?;
+    svg.append_child(&nov)?;
+    let dec = svg_text(document, 241.0, 93.0, "Dec")?;
+    svg.append_child(&dec)?;
+
+    let summer = svg_text(document, 395.0, 270.0, "Summer")?;
+    summer.set_attribute_ns(None, "font-size", "48")?;
+    summer.set_attribute_ns(None, "stroke", "black")?;
+    summer.set_attribute_ns(None, "fill", "gold")?;
+    svg.append_child(&summer)?;
+
+    let winter = svg_text(document, 420.0, 760.0, "Winter")?;
+    winter.set_attribute_ns(None, "font-size", "48")?;
+    winter.set_attribute_ns(None, "stroke", "black")?;
+    winter.set_attribute_ns(None, "fill", "white")?;
+    svg.append_child(&winter)?;
+
+    let spring = svg_text(document, 180.0, 515.0, "Spring")?;
+    spring.set_attribute_ns(None, "font-size", "48")?;
+    spring.set_attribute_ns(None, "stroke", "black")?;
+    spring.set_attribute_ns(None, "fill", "green")?;
+    svg.append_child(&spring)?;
+
+    let fall = svg_text(document, 720.0, 515.0, "Fall")?;
+    fall.set_attribute_ns(None, "font-size", "48")?;
+    fall.set_attribute_ns(None, "stroke", "black")?;
+    fall.set_attribute_ns(None, "fill", "brown")?;
+    svg.append_child(&fall)?;
+
+    let (nowx, nowy) = calpoint(now);
+    let nowline = svg_line(document, nowx, nowy, 500.0, 500.0,
+                           "red", "3")?;
+    svg.append_child(&nowline)?;
 
     Ok(svg)
 }
@@ -71,6 +131,16 @@ fn svg_line(document: &Document, x: f32, y: f32, x2: f32, y2: f32,
     line.set_attribute_ns(None, "stroke", stroke)?;
     line.set_attribute_ns(None, "stroke-width", stroke_width)?;
     Ok(line)
+}
+
+fn svg_text(document: &Document, x: f32, y: f32, text: &str)
+            -> Result<Element, JsValue>
+{
+    let elem = document.create_element_ns(Some(XMLNS), "text")?;
+    elem.set_attribute_ns(None, "x", &*format!("{}", x))?;
+    elem.set_attribute_ns(None, "y", &*format!("{}", y))?;
+    elem.set_inner_html(text);
+    Ok(elem)
 }
 
 fn calpoint(now: DateTime<Local>) -> (f32, f32) {
